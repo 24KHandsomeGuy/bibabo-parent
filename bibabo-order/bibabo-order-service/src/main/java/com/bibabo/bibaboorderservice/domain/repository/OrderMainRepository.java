7550971172c521605e8d249773b5651c3e0889f5
fukuixiang <fukuixiang@ChunBo.com>
2022-05-09 19:51:39 +0800

    订单系统集成jpa

---
+++ bibabo-order/bibabo-order-service/src/main/java/com/bibabo/bibaboorderservice/domain/repository/OrderMainRepository.java
@@ -0,0 +1,13 @@
+package com.bibabo.bibaboorderservice.domain.repository;
+
+import com.bibabo.bibaboorderservice.domain.OrderMain;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author fukuixiang
+ * @date 2022/5/9
+ * @time 15:59
+ * @description
+ */
+public interface OrderMainRepository extends JpaRepository<OrderMain, Long> {
+}
